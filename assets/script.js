cab839ebe5ab4af6a22aca271bdd3c052ca1438e
Fazlu601 <fazrlu9575@gmail.com>
2023-04-08 00:18:59 +0700

    adding: some function

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,54 @@
+// Fungsi Listener saat Dom di load
 document.addEventListener('DOMContentLoaded', () => {
-    const formSubmit = document.getElementById('form-submit');
-    const math = document.getElementById('math');
-    const result = document.getElementById('result');
-    const buttonReset = document.getElementById('b-reset');
-    const boxAnswer = document.getElementById('answer-box');
+    // Luas Segitiga
+    const formSubmitLuas = document.getElementById('form-submit-luas');
+    const mathLuas = document.getElementById('math-luas');
+    const resultLuas = document.getElementById('result-luas');
+    const buttonResetLuas = document.getElementById('b-reset-luas');
+    const boxAnswerLuas = document.getElementById('answer-box-luas');
+
+    // Keliling segitiga
+    const formSubmitKeliling = document.getElementById('form-submit-keliling');
+    const mathKeliling = document.getElementById('math-keliling');
+    const resultKeliling = document.getElementById('result-keliling');
+    const buttonResetKeliling = document.getElementById('b-reset-keliling');
+    const boxAnswerKeliling = document.getElementById('answer-box-keliling');
     
-    formSubmit.addEventListener('submit', (e) => {
+    // Fungsi Listener saat form Luas Segitiga di submit
+    formSubmitLuas.addEventListener('submit', (e) => {
         const a = document.getElementById('a').value;
         const t = document.getElementById('t').value;
         e.preventDefault();
-        if(boxAnswer.classList.contains('d-none')) {
-            boxAnswer.classList.remove('d-none');
+        if(boxAnswerLuas.classList.contains('d-none')) {
+            boxAnswerLuas.classList.remove('d-none');
         }
-        math.innerText = `1/2 x ${a} x ${t}`;
-        result.innerText = 1/2 * a * t;        
+        mathLuas.innerText = `1/2 x ${a} x ${t}`;
+        resultLuas.innerText = 1/2 * a * t;        
+    })
+    
+    // Fungsi Reset pada form Luas Segitiga
+    buttonResetLuas.addEventListener('click', () => {
+        formSubmitLuas.reset();
+        boxAnswerLuas.classList.add('d-none');
     })
 
-    buttonReset.addEventListener('click', () => {
-        formSubmit.reset();
-        boxAnswer.classList.add('d-none');
+    // Fungsi Listener saat form Keliling Segitiga di submit
+    formSubmitKeliling.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const s1 = document.getElementById('s-a').value;
+        const s2 = document.getElementById('s-b').value;
+        const s3 = document.getElementById('s-c').value;
+        if(boxAnswerKeliling.classList.contains('d-none')) {
+            boxAnswerKeliling.classList.remove('d-none');
+        }
+        mathKeliling.innerText = `${s1} + ${s2} + ${s3}`;
+        resultKeliling.innerText = parseInt(s1) + parseInt(s2) + parseInt(s3);
+    })
+
+        // Fungsi Reset pada form Keliling Segitiga
+    buttonResetKeliling.addEventListener('click', () => {
+        formSubmitKeliling.reset();
+        boxAnswerKeliling.classList.add('d-none');
     })
 
 })
